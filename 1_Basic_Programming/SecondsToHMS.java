825eea777f40dbb67728e49da698ca1ca0819a3e
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-09 14:11:20 +0530

    Create SecondsToHMS.java

---
+++ 1_Basic_Programming/SecondsToHMS.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SecondsToHMS {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+
+        // Input: total seconds
+        int seconds = s.nextInt();
+
+        // Convert seconds to hours, minutes, and seconds
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int tempSeconds = seconds % 60;
+
+        System.out.println("Hours: " + hours + " Minutes: " + minutes + " Seconds: " + tempSeconds);
+    }
+}
+
+/*
+Sample Input:
+3665
+
+Sample Output:
+Hours: 1 Minutes: 1 Seconds: 5
+*/
