b2c36e6b1798898465b17b516ab92ce794717f1e
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-10 12:48:14 +0530

    Update PrintInRows.java

--- 1_Basic_Programming/PrintInRows.java
+++ 1_Basic_Programming/PrintInRows.java
@@ -1,36 +1,27 @@
-import java.util.*;
+// Program to display numbers from 1 to 30 with 5 numbers per row
+class NumbersInRows {
+    public static void main(String[] args) {
+        int count = 1;
 
-public class PrintInRows {
-    public static void main(String args[]) {
-        Scanner s = new Scanner(System.in);
+        // Loop from 1 to 30
+        for (int i = 1; i <= 30; i++) {
+            System.out.print(i + "\t"); // print number with tab space
 
-        System.out.print("Enter the range of numbers to print (5 numbers per row): ");
-        int num = s.nextInt();
-
-        int count = 0; // Count how many numbers printed in current row
-        for (int i = 1; i <= num; i++) {
-            System.out.print(i + "\t"); // Tab space for better alignment
-            count++;
-
-            if (count == 5) {
-                System.out.println(); // Move to next line after 5 numbers
-                count = 0;
+            // After every 5 numbers, go to the next line
+            if (count % 5 == 0) {
+                System.out.println();
             }
-        }
-
-        // If last line had fewer than 5 numbers, print a newline
-        if (count != 0) {
-            System.out.println();
+            count++;
         }
     }
 }
 
 /*
-Sample Input:
-13
-
 Sample Output:
-1    2    3    4    5    
-6    7    8    9    10   
-11   12   13
+1	2	3	4	5	
+6	7	8	9	10	
+11	12	13	14	15	
+16	17	18	19	20	
+21	22	23	24	25	
+26	27	28	29	30
 */
