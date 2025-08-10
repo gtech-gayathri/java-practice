bb4307a8a76098715ebd72ca5a0601ebec05a8e5
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-10 14:15:38 +0530

    Create EvenOddDigitOperation.java

---
+++ 1_Basic_Programming/EvenOddDigitOperation.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class EvenOddDigitCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (max 6 digits): ");
+        int num = sc.nextInt();
+
+        int sumEven = 0;
+        int mulOdd = 1;
+
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit % 2 == 0) {
+                sumEven += digit;
+            } else {
+                mulOdd *= digit;
+            }
+            num /= 10;
+        }
+
+        System.out.println("Sum of even digits: " + sumEven);
+        System.out.println("Multiplication of odd digits: " + mulOdd);
+
+        sc.close();
+    }
+}
+
+/*
+Sample Input:
+Enter a number (max 6 digits): 123456
+
+Sample Output:
+Sum of even digits: 12
+Multiplication of odd digits: 15
+*/
