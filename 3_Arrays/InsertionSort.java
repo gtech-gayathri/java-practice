a12750bbefae6a29210530e3d3f0ebda069d24b5
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-11 09:56:19 +0530

    Create InsertionSort.java

---
+++ 3_Arrays/InsertionSort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Insertion Sort Logic
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        System.out.println("Sorted array (Insertion Sort): " + Arrays.toString(arr));
+    }
+}
+
+/*
+Sample Input:
+5
+12 9 21 17 5
+
+Sample Output:
+Sorted array (Insertion Sort): [5, 9, 12, 17, 21]
+*/
