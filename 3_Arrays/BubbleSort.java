dcae0240e326bcc58e9174103e724521e92d757b
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-11 09:51:17 +0530

    Create BubbleSort.java

---
+++ 3_Arrays/BubbleSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort Logic
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted array:" + Arrays.toString(arr));
+    }
+}
+
+/*
+Sample Input:
+Enter number of elements: 5
+Enter 5 elements:
+64 34 25 12 22
+
+Sample Output:
+Sorted array: [12, 22, 25, 34, 64]
+*/
