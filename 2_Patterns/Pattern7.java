d082a04cba99d1b54d4548e0ec7ad7227f365492
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-13 19:44:59 +0530

    Create Pattern7.java

---
+++ 2_Patterns/Pattern7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j >= n - i + 1; j--) {
+                System.out.print(j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Sample Input/Output:
+Enter the number of rows: 5
+5   4   3   2   1
+5   4   3   2
+5   4   3
+5   4
+5
+*/
