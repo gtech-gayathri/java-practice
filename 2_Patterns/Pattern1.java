c6b40e7cea6c27e238d70a493c8e9be6f75debdd
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-13 19:34:08 +0530

    Create Pattern1.java

---
+++ 2_Patterns/Pattern1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "\t"); // \t for tab spacing
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Sample Input/Output:
+Enter the number of rows: 5
+1
+1   2
+1   2   3
+1   2   3   4
+1   2   3   4   5
+*/
