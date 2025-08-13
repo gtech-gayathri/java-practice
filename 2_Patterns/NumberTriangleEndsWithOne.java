f1b1b4b3b52f3fd796740a3f511b19484e3140e5
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-13 20:54:03 +0530

    Update and rename Pattern3.java to NumberTriangleEndsWithOne.java

--- 2_Patterns/NumberTriangleEndsWithOne.java
+++ 2_Patterns/NumberTriangleEndsWithOne.java
@@ -1,5 +1,6 @@
 import java.util.*;
-public class Pattern3 {
+
+public class NumberTriangleEndsWithOne {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
