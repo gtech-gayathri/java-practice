5d28404a0b4a25ee016457ca45504b8c44b57ff2
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-14 09:47:46 +0530

    Update and rename Pattern9.java to RightAlignedNumberTriangle .java

--- 2_Patterns/RightAlignedNumberTriangle .java
+++ 2_Patterns/RightAlignedNumberTriangle .java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Pattern9 {
+public class RightAlignedNumberTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -7,11 +7,11 @@ public class Pattern9 {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            // Print leading spaces for right alignment
+            // Print leading tabs for alignment
             for (int s = 1; s < i; s++) {
                 System.out.print("\t");
             }
-            // Print repeated numbers
+            // Print the same number 'i' in each row, decreasing in count each time
             for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print(i + "\t");
             }
@@ -21,8 +21,7 @@ public class Pattern9 {
 }
 
 /*
-Sample Input/Output:
-Enter the number of rows: 5
+Sample Output for n = 5:
 1	1	1	1	1
 	2	2	2	2
 		3	3	3
