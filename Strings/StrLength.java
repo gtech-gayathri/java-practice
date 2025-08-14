b6bcc7697b34a70dea306aa7b71bd3318e71138f
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-14 10:18:40 +0530

    Create StrLength.java

---
+++ Strings/StrLength.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StrLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+        
+        System.out.println("Length of the string: " + count);
+    }
+}
