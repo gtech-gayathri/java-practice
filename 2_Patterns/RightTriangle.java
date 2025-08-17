1746a11d3fc1f5e1f386f36512b1a8ab16718222
gtech-gayathri <111073870+gtech-gayathri@users.noreply.github.com>
2025-08-17 19:16:10 +0530

    Update RightTriangle.java

--- 2_Patterns/RightTriangle.java
+++ 2_Patterns/RightTriangle.java
@@ -16,6 +16,27 @@ public class RightTriangle {
     }
 }
 
+//We can also write the code as follows to get right triangle
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (j <= rows - i) {
+                    System.out.print("  "); // spaces
+                } else {
+                    System.out.print("* "); // stars
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
 /*
 Output:
         * 
